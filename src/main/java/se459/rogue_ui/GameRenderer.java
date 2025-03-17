f58ff0810423f0f5b17a352f8ff51cf8ff2819f0
PavanBoda <88265241+PavanBoda@users.noreply.github.com>
2025-03-17 12:35:47 -0500

    Updated code for GameRenderer

--- src/main/java/se459/rogue_ui/GameRenderer.java
+++ src/main/java/se459/rogue_ui/GameRenderer.java
@@ -1,30 +1,32 @@
 package se459.rogue_ui;
-
+ 
 import java.util.List;
-
+ 
 import se459.rogue_data.Armor;
 import se459.rogue_data.Dungeon;
 import se459.rogue_data.Food;
 import se459.rogue_data.Gold;
 import se459.rogue_data.Item;
 import se459.rogue_data.Player;
+import se459.rogue_data.Potion;
+import se459.rogue_data.Scroll;
 import se459.rogue_data.Weapon;
 import se459.rogue_data.Monster;
-
+ 
 public class GameRenderer {
-
+ 
     public static void render(Dungeon dungeon, Player player, Monster monster, String gameMessages, String playerStats) {
         clearScreen();
         System.out.println(gameMessages);
-
+ 
         char[][] grid = dungeon.getGrid();
         char[][] displayGrid = new char[grid.length][grid[0].length];
-
+ 
         // Copy the base dungeon grid
         for (int y = 0; y < grid.length; y++) {
             System.arraycopy(grid[y], 0, displayGrid[y], 0, grid[y].length);
         }
-
+ 
         // Render items (Food, Weapons, Armor, Gold)
         List<Item> items = dungeon.getItems();
         for (Item item : items) {
@@ -36,9 +38,15 @@ public class GameRenderer {
                 displayGrid[item.getY()][item.getX()] = '^';
             } else if (item instanceof Gold) {
                 displayGrid[item.getY()][item.getX()] = '$';
+            } else if (item instanceof Scroll) {
+                displayGrid[item.getY()][item.getX()] = '&';
+            } else if (item instanceof Potion) {
+                displayGrid[item.getY()][item.getX()] = '!';
             }
+ 
+ 
         }
-
+ 
         // Place the monster on the display grid if it's alive
         if (monster != null && monster.isAlive()) {
             displayGrid[monster.getY()][monster.getX()] = 'E';
@@ -47,10 +55,10 @@ public class GameRenderer {
             displayGrid[monster.getY()][monster.getX()] = '.';
             dungeon.setTile(monster.getX(), monster.getY(), '.');
         }
-
+ 
         // Place the player, overriding any tile
         displayGrid[player.getY()][player.getX()] = '@';
-
+ 
         // Print the dungeon with the updated display grid
         for (int y = 0; y < displayGrid.length; y++) {
             for (int x = 0; x < displayGrid[y].length; x++) {
@@ -58,10 +66,10 @@ public class GameRenderer {
             }
             System.out.println();
         }
-
+ 
         System.out.println("Player Stats: " + playerStats);
     }
-
+ 
     private static void clearScreen() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
@@ -74,5 +82,5 @@ public class GameRenderer {
             System.out.println("Failed to clear screen");
         }
     }
-    
+   
 }
